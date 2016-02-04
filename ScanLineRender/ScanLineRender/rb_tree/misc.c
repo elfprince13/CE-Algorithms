93378c6422a1f44493243ecddcac3050d46a39e0
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2016-02-04 11:24:11 -0500

    forgot 84PCE includes

--- ScanLineRender/ScanLineRender/rb_tree/misc.c
+++ ScanLineRender/ScanLineRender/rb_tree/misc.c
@@ -1,34 +1,42 @@
 #include <stdlib.h>
 #include "misc.h"
-#include "../debugConfig.h"
+#include "debugConfig.h"
 
 
 /***********************************************************************/
 /*  FUNCTION:  SafeMalloc */
 /**/
-/*    INPUTS:  size is the size to SafeMalloc */
+/*    INPUTS:  size is the size to malloc */
 /**/
 /*    OUTPUT:  returns pointer to allocated memory if succesful */
 /**/
-/*    EFFECT:  SafeMallocs new memory.  If SafeMalloc fails, prints error message */
+/*    EFFECT:  mallocs new memory.  If malloc fails, prints error message */
 /*             and terminates program. */
 /**/
 /*    Modifies Input: none */
 /**/
 /***********************************************************************/
 
+#ifdef _EZ80
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <ti84pce.h>
+extern void cleanUp(void);
+#endif
+
 void * SafeMalloc(size_t size) {
 	void *const mem = malloc(size);
-
-  if ( !mem ) {
+	
+	if ( !mem ) {
 #ifdef _EZ80
-	  cleanUp();
-	  _OS( asm("JP _ErrMemory") );
+		cleanUp();
+		_OS( asm("JP _ErrMemory") );
 #else
-	  dPrintf(("memory overflow: SafeMalloc failed in SafeMalloc."));
-	  dPrintf(("  Exiting Program.\n"));
-	  exit(-1);
+		dPrintf(("memory overflow: SafeMalloc failed in SafeMalloc."));
+		dPrintf(("  Exiting Program.\n"));
+		exit(-1);
 #endif
-  }
+	}
 	return mem;
 }
