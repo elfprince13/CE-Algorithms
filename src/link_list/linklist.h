887a7b5d42fcb57a1bebba19d80395834c6491a3
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2016-02-08 17:40:58 -0500

    link list documentation updated

--- src/link_list/linklist.h
+++ src/link_list/linklist.h
@@ -11,22 +11,54 @@
 
 #include <stdint.h>
 
+/***************************************************************************************
+ * The base type for nodes that belong in a linked-list
+ ***************************************************************************************/
 typedef struct _LinkN {
 	void * data;
 	struct _LinkN *tail;
 } LinkN;
 
-typedef int8_t(*CompareF)(void *, void*, void*);
+/***************************************************************************************
+ * CompareF compares two data references, and may optionally make use of additional state
+ ***************************************************************************************/
+typedef int8_t(*CompareF)(const void *, const void*, void*);
+
+/***************************************************************************************
+ * The comparator type encapsulates CompareF, and state to be passed through
+ ***************************************************************************************/
 typedef struct {
 	CompareF f;
 	void * state;
 } Comparator;
 
+/***************************************************************************************
+ * Given the head of a list, and a comparator for its data,
+ * modifies the list to be in sorted order using the merge sort algorithm
+ ***************************************************************************************/
 void mergeSort(LinkN** headRef, const Comparator *comparator);
+
+/***************************************************************************************
+ * Frees a single link, and optionally, the data.
+ ***************************************************************************************/
 void freeLink(LinkN *link, void(*freeData)(void*));
+
+/***************************************************************************************
+ * Frees a whole list, and optionally, the data.
+ ***************************************************************************************/
 void freeList(LinkN *link, void(*freeData)(void*));
 
+/***************************************************************************************
+ * Points prev->tail to target->tail, unless prev is null
+ * in which case headRef is updated instead.
+ * Returns the link that that was removed (note: target->tail is not modified)
+ ***************************************************************************************/
 LinkN* removeLink(LinkN **headRef, LinkN* target, LinkN* prev);
+
+/***************************************************************************************
+ * Updates headRef so that target is the new head, and target->tail points to the old
+ * head. The previous value of target->tail is returned.
+ ***************************************************************************************/
 LinkN* linkFront(LinkN **headRef, LinkN* target);
 
 
